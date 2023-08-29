0d607dd12fa223dc9a4c61d047a4a42b00fb77fc
pmedia84 <80710345+pmedia84@users.noreply.github.com>
2023-08-29 10:12:48 +0100

    pl

--- price_list.php
+++ price_list.php
@@ -76,7 +76,7 @@ $cat_r = mysqli_query($db, $cat_q);
                                 <div class="price-list-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
                                     <meta itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
                                     <h4><span itemprop="name"><?= $service['service_name']; ?></span><span itemprop="price"><span itemprop="priceCurrency" content="GBP">&#163;</span><?= $service['service_price']; ?></h4>
-                                    <p itemprop="description"><?= html_entity_decode($service['service_description']) ; ?></p>
+                                    <div itemprop="description"><?= html_entity_decode($service['service_description']) ; ?></div>
                                 </div>
                             <?php endforeach; ?>
                     <?php endforeach;
--- scripts/price_list.script.php
+++ scripts/price_list.script.php
@@ -1,4 +1,3 @@
-
 <?php
 //price list controls from POST request Filter Select
 if (isset($_POST['action']) && $_POST['action'] == "price_list_filter") :
@@ -15,15 +14,16 @@ if (isset($_POST['action']) && $_POST['action'] == "price_list_filter") :
         ?>
                 <h3><?= $cat['service_cat_name']; ?></h3>
                 <?php foreach ($service_q as $service) : ?>
-                    <div class="price-list-item">
-                        <h4><?= $service['service_name']; ?><span>&#163;<?= $service['service_price']; ?></h4>
-                        <p><?= $service['service_description']; ?></p>
+                    <div class="price-list-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
+                        <meta itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
+                        <h4><span itemprop="name"><?= $service['service_name']; ?></span><span itemprop="price"><span itemprop="priceCurrency" content="GBP">&#163;</span><?= $service['service_price']; ?></h4>
+                        <div itemprop="description"><?= html_entity_decode($service['service_description']); ?></div>
                     </div>
                 <?php endforeach; ?>
         <?php endforeach;
         endif; ?>
     <?php else :
-    include("../connect.php");
+        include("../connect.php");
         //define category
         $search = mysqli_real_escape_string($db, $_POST['service_cat_id']);
         //find services details
@@ -36,9 +36,10 @@ if (isset($_POST['action']) && $_POST['action'] == "price_list_filter") :
     ?>
         <h3><?= $category_r['service_cat_name']; ?></h3>
         <?php foreach ($services_q as $service) : ?>
-            <div class="price-list-item">
-                <h4><?= $service['service_name']; ?><span>&#163;<?= $service['service_price']; ?></h4>
-                <p><?= $service['service_description']; ?></p>
+            <div class="price-list-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
+                <meta itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
+                <h4><span itemprop="name"><?= $service['service_name']; ?></span><span itemprop="price"><span itemprop="priceCurrency" content="GBP">&#163;</span><?= $service['service_price']; ?></h4>
+                <div itemprop="description"><?= html_entity_decode($service['service_description']); ?></div>
             </div>
         <?php endforeach; ?>
     <?php endif; ?>
@@ -60,27 +61,29 @@ if (isset($_POST['action']) && $_POST['action'] == "price_list_filter") :
         ?>
                 <h3><?= $cat['service_cat_name']; ?></h3>
                 <?php foreach ($service_q as $service) : ?>
-                    <div class="price-list-item">
-                        <h4><?= $service['service_name']; ?><span>&#163;<?= $service['service_price']; ?></h4>
-                        <p><?= $service['service_description']; ?></p>
+                    <div class="price-list-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
+                        <meta itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
+                        <h4><span itemprop="name"><?= $service['service_name']; ?></span><span itemprop="price"><span itemprop="priceCurrency" content="GBP">&#163;</span><?= $service['service_price']; ?></h4>
+                        <div itemprop="description"><?= html_entity_decode($service['service_description']); ?></div>
                     </div>
                 <?php endforeach; ?>
         <?php endforeach;
         endif; ?>
 
     <?php else :
-    include("../connect.php");
+        include("../connect.php");
         $search = mysqli_real_escape_string($db, $_POST['search']);
         $service_q = $db->query('SELECT services.service_name, services.service_description, services.service_cat_id, services.service_price, services_categories.service_cat_id, services_categories.service_cat_name FROM services LEFT JOIN services_categories ON services_categories.service_cat_id=services.service_cat_id WHERE services.service_name  LIKE "%' . $search . '%" ');
-            $service_r = mysqli_fetch_assoc($service_q); ?>
-            <p><?php echo mysqli_num_rows($service_q);?> Results found, matching <?=$search;?></p>
-            <?php foreach ($service_q as $service) : ?>
-                <h3><?= $service['service_cat_name']; ?></h3>
-                
-                <div class="price-list-item">
-                    <h4><?= $service['service_name']; ?><span>&#163;<?= $service['service_price']; ?></h4>
-                    <p><?= $service['service_description']; ?></p>
-                </div>
-            <?php endforeach; ?>
+        $service_r = mysqli_fetch_assoc($service_q); ?>
+        <p><?php echo mysqli_num_rows($service_q); ?> Results found, matching <?= $search; ?></p>
+        <?php foreach ($service_q as $service) : ?>
+            <h3><?= $service['service_cat_name']; ?></h3>
+
+            <div class="price-list-item" itemprop="itemListElement" itemscope itemtype="https://schema.org/Offer">
+                <meta itemprop="itemOffered" itemscope itemtype="https://schema.org/Service">
+                <h4><span itemprop="name"><?= $service['service_name']; ?></span><span itemprop="price"><span itemprop="priceCurrency" content="GBP">&#163;</span><?= $service['service_price']; ?></h4>
+                <div itemprop="description"><?= html_entity_decode($service['service_description']); ?></div>
+            </div>
+        <?php endforeach; ?>
     <?php endif; ?>
 <?php endif; ?>
